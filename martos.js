572cd348230705e2b70ad90e4641be1b05105e97
MiniT0on <70152813+MiniT0on@users.noreply.github.com>
2021-08-22 15:47:36 -0500

    Update martos.js

--- martos.js
+++ martos.js
@@ -1,1 +1,1 @@
-var fax1=document.getElementById('fax1');function fuc1(){fax1.style.top="0"+"%";};function fuc2(){fax1.style.top="100"+"%";};document.getElementById('BB1').onclick=fuc1;
+var fax1=document.getElementById('fax1');function fuc1(){fax1.style.top="0"+"%";};function fuc2(){fax1.style.top="100"+"%";};document.getElementById('BB1').onclick=fuc1;document.getElementById('BB2').onclick=fuc2;
