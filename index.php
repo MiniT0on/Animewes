85a7b9817f2b3d7a2269f88a7b4b0615cca32482
MiniT0on <70152813+MiniT0on@users.noreply.github.com>
2021-10-10 10:27:05 -0500

    Create index.php

---
+++ index.php
@@ -0,0 +1,8 @@
+<?php
+
+if (isset($_GET['nombre']) && isset($_GET['email']) && isset($_GET['mensagge'])) {
+	mail("AdrianoMegapro@gmail.com", "ERES UN HIJODE PUTA", "message");
+	echo "Se mando tu maldito mensaje ONI-CHAN HENTAI >:V";
+}
+
+?>
